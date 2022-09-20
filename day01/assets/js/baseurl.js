b84f355778280459496968ef45a6c2785b7f399a
xsnb <1470656667@qq.com>
2022-09-20 16:58:41 +0800

    完成了主页的开发

--- day01/assets/js/baseurl.js
+++ day01/assets/js/baseurl.js
@@ -14,7 +14,7 @@ $.ajaxPrefilter(function (options) {
     // 全局统一挂载 complete 回调函数
     // 不论成功还是失败，最终都会调用 complete 回调函数
     options.complete = res => {
-        // console.log('执行了')
+        // consolegit.log('执行了')
         // console.log(res)
         // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
         if (res.responseJSON.status == '1' && res.responseJSON.message === '身份认证失败！') {
