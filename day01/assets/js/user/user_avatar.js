413c302a6df9e661e6153e6a00aa9e69acbfa645
xsnb <1470656667@qq.com>
2022-09-22 08:54:28 +0800

    完成了头像区域

--- day01/assets/js/user/user_avatar.js
+++ day01/assets/js/user/user_avatar.js
@@ -35,4 +35,33 @@ $('#file').on('change',function(e){
   .cropper('destroy') // 销毁旧的裁剪区域
   .attr('src', imgURL) // 重新设置图片路径
   .cropper(options) // 重新初始化裁剪区域
+
+
+  // 为确定按钮，添加点击事件
+  $('#btnUpLoad').on('click',function(){
+    // 要拿到用户裁剪之后的头像
+    let dataURL = $image
+    .cropper('getCroppedCanvas', {
+      // 创建一个 Canvas 画布
+      width: 100,
+      height: 100
+    })
+    .toDataURL('image/png')
+    // 创建一个 Canvas 画布，将 Canvas 画布上的内容，转化为 base64 格式的字符串
+  
+
+  $.ajax({
+    method : 'POST',
+    url : '/my/update/avatar',
+    data : {
+      avatar : dataURL ,
+    },
+    success : res => {
+      if(res.status !== 0) return layer.msg('头像上传失败！')
+      layer.msg('头像上传成功！')
+      //  调用父窗口中的渲染页面函数 重新渲染页面
+      window.parent.getUserInfo()
+    }
+  })
+})
 })
